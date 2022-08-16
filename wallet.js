9c0e9fa56efb070633416fd155c951460277ec66
maglangitsuprian <maglangitsuprian@gmail.com>
2022-08-16 19:01:15 +0800

    added many features
    added coin price
    added coin price change in 24hours
    added buy button functionality

--- wallet.js
+++ wallet.js
@@ -1,4 +1,3 @@
-
 let bitcoin = $(".bitcoin-price")
 let ethereum = $(".ethereum-price")
 let tether = $(".tether-price")
@@ -10,12 +9,25 @@ let usdtChange = $(".tether-change")
 let currentBalance = 0
 let totalBalance = $(".total-balance")
 
+let bitcoinQuantity = $(".bitcoin-quantity")
+let ethereumQuantity = $(".ethereum-quantity")
+let tetherQuantity = $(".tether-quantity")
+
+let bitcoinQuantityNum = 0
+let ethereumQuantityNum = 0
+let tetherQuantityNum = 0
+
+let bitcoinValue = $(".bitcoin-value")
+let ethereumValue = $(".ethereum-value")
+let tetherValue = $(".tether-value")
 
 
 const main = ()=> {
-    cryptoPrices()
+    
     deposit()
     withdraw()
+    buy()
+    cryptoPrices()
 }
 
 
@@ -37,17 +49,16 @@ const cryptoPrices = ()=>{
             btcChange.html(`${bitcoinChange.toFixed(2)}%`)
             ethChange.html(`${ethereumChange.toFixed(2)}%`)
             usdtChange.html(`${tetherChange.toFixed(2)}%`)
-            
+            cryptoValue()
+            overallBalance()
         }, 1000)
-        
-
 }
 
 
 const deposit = () => {
     $(".deposit-crypto").on("click", ()=>{
-     currentBalance = currentBalance +  parseInt($(".deposit-amount").val())
-     totalBalance.html(`$${currentBalance}`)
+    tetherQuantityNum = tetherQuantityNum + parseInt($(".deposit-amount").val()) 
+    tetherQuantity.html(`${tetherQuantityNum} USDT`)
     })
     
 }
@@ -56,9 +67,58 @@ const deposit = () => {
 const withdraw = () => {
     $(".withdraw-crypto").on("click", ()=>{
      currentBalance = currentBalance -  parseInt($(".withdraw-amount").val())
-     totalBalance.html(`$${currentBalance}`)
     })
+}
+
+
+const buy = ()=> {
+    $(".buy-button").on("click", ()=>{
+     let value = parseInt($("#amount").val())
+     let selectedCoin = $("select").val()
+
+     if(currentBalance>=value  && tetherQuantityNum>=value){
+        if(selectedCoin === "bitcoin"){
+            bitcoinQuantityNum = (bitcoinQuantityNum + (value / bitcoin.html().slice(1))) 
+            bitcoinQuantity.html(`${bitcoinQuantityNum.toFixed(5)} BTC`)
+            tetherQuantityNum = tetherQuantityNum - value
+            tetherQuantity.html(`${tetherQuantityNum} USDT`)
+
+        } else if (selectedCoin === "ethereum"){
+            ethereumQuantityNum = (ethereumQuantityNum + (value / ethereum.html().slice(1))) 
+            ethereumQuantity.html(`${ethereumQuantityNum.toFixed(5)} ETH`) 
+            tetherQuantityNum = tetherQuantityNum - value
+            tetherQuantity.html(`${tetherQuantityNum} USDT`)
+        } else {
+            alert("Select coin")
+        }
+     } else {
+        alert("Sorry, Not enough balance! Please deposit USDT.")
+     }})
+}
+
+
+// const sell = ()=> {
+//     $(".sell-button").on("click", ()=>{
+//      let value = parseInt($("#amount").val())
+//      let selectedCoin = $("select").val()
     
+//     if(selectedCoin === "bitcoin"){
+//         bitcoinQuantityNum
+//     } 
+//     })
+// }
+
+
+const cryptoValue = ()=>{
+    bitcoinValue.html(`$${(bitcoinQuantityNum * parseInt(bitcoin.html().slice(1))).toFixed(2)}`)
+    ethereumValue.html(`$${(ethereumQuantityNum * parseInt(ethereum.html().slice(1))).toFixed(2)}`)
+    tetherValue.html(`$${(tetherQuantityNum * parseInt(tether.html().slice(1))).toFixed(2)}`)
+}
+
+
+const overallBalance = ()=>{
+    currentBalance = parseInt(bitcoinValue.html().slice(1)) + parseInt(ethereumValue.html().slice(1)) + parseInt(tetherValue.html().slice(1))
+    totalBalance.html(`$${currentBalance}`)
 }
 
 
